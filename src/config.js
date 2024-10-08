287d5fae2da0970a8e22c21f3ee2660e901005ea
SATHYA STORE <164324982+sathya01lllll@users.noreply.github.com>
2024-10-08 22:55:47 +0800

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,163 +1,167 @@
+import optional from './images/IMG-20241004-WA0003.jpg';
+import panel from './images/panel.jpg';
+import sc from './images/sc.jpg';
+
 const config = {
-  owner: "6281329731976", // NOMOR WA LU
-  siteName: "𝘚𝘈𝘛𝘏𝘠𝘈 𝘚𝘛𝘖𝘙𝘌", // NAMA STORE MU
-  products: [
-    { // SALIN DARI SINI
-      id: 1, // ID PRODUK MU (TERSERAH YG PENTING NOMER BUKAN HURUF)
-      name: "Promo ADMIN PANEL", // NAMA PRODUK MU
-      price: "Rp 25.000", // HARGA PRODUK MU
-      description: "Special promo for ADMIN PANEL.", // DESKRIPSI PRODUK MU
-      image: "/images/IMG-20241004-WA0003.jpg" // GAMBAR PRODUK MU
-    }, // SAMPAI SINI
+  owner: "6281329731976",
+  // NOMOR WA LU
+  siteName: "𝘚𝘈𝘛𝘏𝘠𝘈 𝘚𝘛𝘖𝘙𝘌",
+  // NAMA STORE MU
+  products: [{
+    id: 1,
+    name: "Promo ADMIN PANEL",
+    price: "Rp 25.000",
+    description: "Special promo for ADMIN PANEL.",
+    image: optional
+  },
     {
       id: 2,
       name: "PT PANEL",
       price: "Rp 90.000",
       description: "Professional tier for PANEL services.",
-      image: "/images/IMG-20241004-WA0003.jpg"
+      image: optional
     },
     {
       id: 3,
       name: "Private Panel 2GB",
       price: "Rp 2.000/Bulan",
       description: "Monthly package for 2GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 4,
       name: "Private Panel 3GB",
       price: "Rp 3.000/Bulan",
       description: "Monthly package for 3GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 5,
       name: "Private Panel 4GB",
       price: "Rp 4.000/Bulan",
       description: "Monthly package for 4GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 6,
       name: "Private Panel 5GB",
       price: "Rp 6.000/Bulan",
       description: "Monthly package for 5GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 7,
       name: "Private Panel 6GB",
       price: "Rp 7.000/Bulan",
       description: "Monthly package for 6GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 8,
       name: "Private Panel 7GB",
       price: "Rp 8.000/Bulan",
       description: "Monthly package for 7GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 9,
       name: "Private Panel 8GB",
       price: "Rp 9.000/Bulan",
       description: "Monthly package for 8GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 10,
       name: "Private Panel 9GB",
       price: "Rp 10.000/Bulan",
       description: "Monthly package for 9GB private panel.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 11,
       name: "Private Panel UNLIMITED",
       price: "Rp 29.000/Bulan",
       description: "Unlimited monthly panel package.",
-      image: "/images/panel.jpg"
+      image: panel
     },
     {
       id: 12,
       name: "SC BUG",
       price: "Rp 35.000",
       description: "SC BUG for WhatsApp.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 13,
       name: "SC PUSHKONTAK",
       price: "Rp 30.000",
       description: "SC PUSHKONTAK script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 14,
       name: "SC JPM",
       price: "Rp 30.000",
       description: "SC JPM script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 15,
       name: "SC JAGA GC",
       price: "Rp 30.000",
       description: "SC JAGA GC script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 16,
       name: "SC MD V9",
       price: "Rp 100.000",
       description: "SC MD V9 script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 17,
       name: "SC MD V19",
       price: "Rp 200.000",
       description: "SC MD V19 script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 18,
       name: "SC STORE",
       price: "Rp 30.000",
       description: "SC STORE script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 19,
       name: "SC BUTTON",
       price: "Rp 30.000",
       description: "SC BUTTON script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 20,
       name: "SC CPANEL V1",
       price: "Rp 35.000",
       description: "SC CPANEL V1 script.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 21,
       name: "SC CPANEL + NOKOS",
       price: "Rp 50.000",
       description: "SC CPANEL combined with NOKOS.",
-      image: "/images/sc.jpg"
+      image: sc
     },
     {
       id: 22,
       name: "SC SPAM PAIRING CODE",
       price: "Rp 50.000",
       description: "SC for SPAM PAIRING CODE.",
-      image: "/images/sc.jpg"
-    }
+      image: sc
+    },
   ],
-
   additionalInfo: {
     pricing: {
       publicPanel: {
@@ -166,10 +170,10 @@ const config = {
           "Hemat Kuota/Penyimpanan",
           "Bot On 24 Jam",
           "Panel Fast",
-          "60% Anti Lag",
+          "60% Anti Leg",
           "Spek RAM 8 Core 4",
-          "Garansi 15 Hari",
-          "Dan Lain-lain"
+          "Garansi 15 Day",
+          "DLL"
         ]
       },
       privatePanel: {
@@ -185,9 +189,9 @@ const config = {
           "Bot Lebih Fast Respons",
           "Run Bot 24 Jam",
           "Hemat Kuota/Penyimpanan",
-          "Garansi FULL [15 Hari]",
+          "Garansi FULL [15 DAY]",
           "Free Pengajaran Run Bot",
-          "Dibimbing Langsung Oleh Owner"
+          "Dipahtau Langsung Oleh Owner"
         ]
       }
     },
